c47da15d0f6802fcd25b135841b45be33ea3a1d1
TDTaiii2005 <taductai123123qa@gmail.com>
2026-08-13 08:46:37 +0700

    basic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayout } from './shared/header-layout/header-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-basic-project');
+  //text
+  title = {
+    name: 'Angular Basic Project',
+    old: 2024
+  }
+
+
+  //Propoties
+  isDisabled = false;
+
+  //attributes
+
+  contentImage = 'hello, day la image';
 }
---
+++ src/app/shared/header-layout/header-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-layout',
+    standalone: true,
+    templateUrl: './header-layout.html',
+    styleUrl: './header-layout.css'
+})
+
+export class HeaderLayout {
+}
